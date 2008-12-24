d7d037002f97c71ced47bff7bdb422d2f52ee9c6
kalith <kalith@7870209e-198f-4d4d-96a2-d6e540253c14>
2008-12-24 02:26:42 +0000

    Rewritten XML validation, rewritten Lua and C++ interraction for the UI, started working on XML UI parsing.

---
+++ src/gui/frost_button_glues.cpp
@@ -0,0 +1,19 @@
+/* ###################################### */
+/* ###     Frost Engine, by Kalith    ### */
+/* ###################################### */
+/*                                        */
+
+#include "gui/frost_gui_button.h"
+
+#include "gui/frost_gui_frame.h"
+
+using namespace std;
+using namespace Frost;
+using namespace Frost::GUI;
+
+const s_str LuaButton::CLASS_NAME = "GUI::LuaButton";
+
+LuaButton::LuaButton(lua_State* pLua) : LuaFrame(pLua)
+{
+    pButtonParent_ = (Button*)(pParent_.Get());
+}
---
+++ src/gui/frost_layeredregion_glues.cpp
@@ -0,0 +1,19 @@
+/* ###################################### */
+/* ###     Frost Engine, by Kalith    ### */
+/* ###################################### */
+/*                                        */
+
+#include "gui/frost_gui_layeredregion.h"
+
+#include "gui/frost_gui_uiobject.h"
+
+using namespace std;
+using namespace Frost;
+using namespace Frost::GUI;
+
+const s_str LuaLayeredRegion::CLASS_NAME = "GUI::LuaLayeredRegion";
+
+LuaLayeredRegion::LuaLayeredRegion(lua_State* pLua) : LuaUIObject(pLua)
+{
+    pLayeredRegionParent_ = (LayeredRegion*)(pParent_.Get());
+}
