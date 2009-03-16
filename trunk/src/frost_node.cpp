8189da73453c63165b5d103514bc3724f3e1c432
kalith <kalith@7870209e-198f-4d4d-96a2-d6e540253c14>
2009-03-16 02:28:08 +0000

    First commit.

---
+++ trunk/src/frost_node.cpp
@@ -0,0 +1,35 @@
+/* ###################################### */
+/* ###     Frost Engine, by Kalith    ### */
+/* ###################################### */
+/*              Node source               */
+/*                                        */
+/*                                        */
+
+#include "frost_node.h"
+
+#include <OgreSceneNode.h>
+
+using namespace std;
+
+namespace Frost
+{
+    Node::Node( const s_uint& uiID, const Vector& mPos )
+    {
+        uiID_ = uiID;
+        pNode_->setPosition(Vector::FrostToOgre(mPos));
+    }
+
+    Node::~Node()
+    {
+    }
+
+    s_ptr<Ogre::SceneNode> Node::GetOgreNode()
+    {
+        return pNode_;
+    }
+
+    const s_uint& Node::GetID()
+    {
+        return uiID_;
+    }
+}
