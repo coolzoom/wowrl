55d14ac690fbeff5a239f8ec5a38ca56d9d24265
kalith <kalith@7870209e-198f-4d4d-96a2-d6e540253c14>
2009-06-28 20:50:37 +0000

    Added atan2() to Utils' math.h

--- trunk/include/utils/frost_utils_math.h
+++ trunk/include/utils/frost_utils_math.h
@@ -18,6 +18,7 @@ namespace Frost
     s_float asin(const s_int& iValue);
     s_float tan(const s_int& iValue);
     s_float atan(const s_int& iValue);
+    s_float atan2(const s_int& iY, const s_int& iX);
     s_int   fabs(const s_int& iValue);
     s_float pow(const s_int& iValue, const s_float& fValue);
     s_float pow(const s_int& iValue, const s_double& dValue);
@@ -31,6 +32,7 @@ namespace Frost
     s_float asin(const s_uint& uiValue);
     s_float tan(const s_uint& uiValue);
     s_float atan(const s_uint& uiValue);
+    s_float atan2(const s_uint& uiY, const s_uint& uiX);
     s_float pow(const s_uint& uiValue, const s_float& fValue);
     s_float pow(const s_uint& uiValue, const s_double& dValue);
     s_float sqrt(const s_uint& uiValue);
@@ -43,6 +45,7 @@ namespace Frost
     s_float asin(const s_float& fValue);
     s_float tan(const s_float& fValue);
     s_float atan(const s_float& fValue);
+    s_float atan2(const s_float& fY, const s_float& fX);
     s_float fabs(const s_float& fValue);
     s_float pow(const s_float& fValue1, const s_float& fValue2);
     s_float pow(const s_float& fValue, const s_double& dValue);
@@ -56,6 +59,7 @@ namespace Frost
     s_double asin(const s_double& dValue);
     s_double tan(const s_double& dValue);
     s_double atan(const s_double& dValue);
+    s_double atan2(const s_double& dY, const s_double& dX);
     s_double fabs(const s_double& dValue);
     s_double pow(const s_double& dValue1, const s_double& dValue2);
     s_double pow(const s_double& dValue, const s_float& fValue);
