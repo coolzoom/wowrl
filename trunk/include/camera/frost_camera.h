0188c1ad68e4c0df2d095f24f884a016bb51f063
kalith <kalith@7870209e-198f-4d4d-96a2-d6e540253c14>
2009-08-15 13:50:11 +0000

    Added Camera::IsVisible() to check is a point/box is visible.

--- trunk/include/camera/frost_camera.h
+++ trunk/include/camera/frost_camera.h
@@ -30,6 +30,24 @@ namespace Frost
         /// Destructor.
         virtual ~Camera();
 
+        /// Checks if a point is visible in this Camera.
+        /** \param mPoint The point to check
+        *   \return 'true' if the point is visible in this Camera
+        */
+        s_bool IsVisible(const Vector& mPoint) const;
+
+        /// Checks if a box is visible in this Camera.
+        /** \param mMin The first point of the box
+        *   \param mMax The second point of the box
+        *   \return 'true' if the box is visible in this Camera
+        */
+        s_bool IsVisible(const Vector& mMin, const Vector& mMax) const;
+
+        /// Returns this Camera's far clip distance.
+        /** \return This Camera's far clip distance
+        */
+        s_float GetFarClipDistance() const;
+
         /// Tells this Camera that it is the main camera.
         /** \param bMainCamera 'true' if its the new main camera
         *   \note You shouldn't call this. Use CameraManager::SetMainCamera().
