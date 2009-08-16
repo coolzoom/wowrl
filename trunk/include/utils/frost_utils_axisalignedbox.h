2ba109ccb374914b286388752328ebbc63052c3d
kalith <kalith@7870209e-198f-4d4d-96a2-d6e540253c14>
2009-08-16 00:30:29 +0000

    Added AxisAlignedBox to the Utils library.

---
+++ trunk/include/utils/frost_utils_axisalignedbox.h
@@ -0,0 +1,85 @@
+/* ###################################### */
+/* ###     Frost Engine, by Kalith    ### */
+/* ###################################### */
+/*          AxisAlignedBox header         */
+/*                                        */
+/*                                        */
+
+
+#ifndef FROST_AXISALIGNEDBOX_H
+#define FROST_AXISALIGNEDBOX_H
+
+#include "frost_utils.h"
+#include "frost_utils_vector.h"
+
+namespace Ogre
+{
+    class AxisAlignedBox;
+}
+
+namespace Frost
+{
+    class AxisAlignedBox
+    {
+    public :
+
+        /// Default constructor.
+        /** \note The default box is infinite.
+        */
+        AxisAlignedBox();
+
+        /// Constructor.
+        /** \param mMin The first point of the box
+        *   \param mMax The second point of the box
+        *   \note mMin's coordinates must be inferior to mMax's.
+        *         These two points are opposite corners of the box.
+        */
+        AxisAlignedBox(const Vector& mMin, const Vector& mMax);
+
+        /// Destructor.
+        ~AxisAlignedBox();
+
+        void SetMin(const Vector& mMin);
+
+        void SetMax(const Vector& mMax);
+
+        const Vector& GetMin() const;
+
+        const Vector& GetMax() const;
+
+        Vector operator[] (const s_uint& uiIndex) const;
+        Vector operator[] (const uint& uiIndex) const;
+
+        /// Checks if this box contains another.
+        /** \param mBox The other box
+        *   \return 'true' if this box contains the other
+        *   \note The box can be partially contained.
+        */
+        s_bool Contains(const AxisAlignedBox& mBox) const;
+
+        /// Checks if this box contains a point.
+        /** \param mPoint The point
+        *   \return 'true' if this box contains the point
+        */
+        s_bool Contains(const Vector& mPoint) const;
+
+        /// Converts a Frost::AxisAlignedBox to an Ogre::AxisAlignedBox.
+        /** \param mBox The box to convert
+        */
+        static Ogre::AxisAlignedBox FrostToOgre(const AxisAlignedBox& mBox);
+
+        /// Converts a Ogre::AxisAlignedBox to an Frost::AxisAlignedBox.
+        /** \param mBox The box to convert
+        */
+        static AxisAlignedBox OgreToFrost(const Ogre::AxisAlignedBox& mBox);
+
+        static const s_str CLASS_NAME;
+
+    private :
+
+        Vector mMin_, mMax_;
+
+    };
+}
+
+#endif
---
+++ trunk/src/utils/frost_utils_axisalignedbox.cpp
@@ -0,0 +1,125 @@
+/* ###################################### */
+/* ###     Frost Engine, by Kalith    ### */
+/* ###################################### */
+/*          AxisAlignedBox source         */
+/*                                        */
+/*                                        */
+
+#include "frost_utils_axisalignedbox.h"
+
+#include <OgreAxisAlignedBox.h>
+
+using namespace std;
+
+namespace Frost
+{
+    AxisAlignedBox::AxisAlignedBox() :
+        mMin_(s_float::INFMINUS), mMax_(s_float::INFPLUS)
+    {
+
+    }
+
+    AxisAlignedBox::AxisAlignedBox(const Vector& mMin, const Vector& mMax) :
+        mMin_(mMin), mMax_(mMax)
+    {
+    }
+
+    AxisAlignedBox::~AxisAlignedBox()
+    {
+    }
+
+    void AxisAlignedBox::SetMin( const Vector& mMin )
+    {
+        mMin_ = mMin;
+    }
+
+    void AxisAlignedBox::SetMax( const Vector& mMax )
+    {
+        mMax_ = mMax;
+    }
+
+    const Vector& AxisAlignedBox::GetMin() const
+    {
+        return mMin_;
+    }
+
+    const Vector& AxisAlignedBox::GetMax() const
+    {
+        return mMax_;
+    }
+
+    Vector AxisAlignedBox::operator[] (const s_uint& uiIndex) const
+    {
+        if (uiIndex == 0)
+            return mMin_;
+        else if (uiIndex == 1)
+            return Vector(mMax_.X(), mMin_.Y(), mMin_.Z());
+        else if (uiIndex == 2)
+            return Vector(mMax_.X(), mMin_.Y(), mMax_.Z());
+        else if (uiIndex == 3)
+            return Vector(mMin_.X(), mMin_.Y(), mMax_.Z());
+        else if (uiIndex == 4)
+            return Vector(mMin_.X(), mMax_.Y(), mMax_.Z());
+        else if (uiIndex == 5)
+            return Vector(mMin_.X(), mMax_.Y(), mMin_.Z());
+        else if (uiIndex == 6)
+            return Vector(mMax_.X(), mMax_.Y(), mMin_.Z());
+        else if (uiIndex == 7)
+            return mMax_;
+        else
+            return Vector::NaN;
+    }
+
+    Vector AxisAlignedBox::operator[] (const uint& uiIndex) const
+    {
+        if (uiIndex == 0)
+            return mMin_;
+        else if (uiIndex == 1)
+            return Vector(mMax_.X(), mMin_.Y(), mMin_.Z());
+        else if (uiIndex == 2)
+            return Vector(mMax_.X(), mMin_.Y(), mMax_.Z());
+        else if (uiIndex == 3)
+            return Vector(mMin_.X(), mMin_.Y(), mMax_.Z());
+        else if (uiIndex == 4)
+            return Vector(mMin_.X(), mMax_.Y(), mMax_.Z());
+        else if (uiIndex == 5)
+            return Vector(mMin_.X(), mMax_.Y(), mMin_.Z());
+        else if (uiIndex == 6)
+            return Vector(mMax_.X(), mMax_.Y(), mMin_.Z());
+        else if (uiIndex == 7)
+            return mMax_;
+        else
+            return Vector::NaN;
+    }
+
+    s_bool AxisAlignedBox::Contains(const AxisAlignedBox& mBox) const
+    {
+        for (uint ui = 0; ui < 8; ++ui)
+        {
+            if (this->Contains(mBox[ui]))
+                return true;
+        }
+
+        return false;
+    }
+
+    s_bool AxisAlignedBox::Contains(const Vector& mPoint) const
+    {
+        return (mPoint.X().IsInRange(mMin_.X(), mMax_.X()) &&
+                mPoint.Y().IsInRange(mMin_.Y(), mMax_.Y()) &&
+                mPoint.Z().IsInRange(mMin_.Z(), mMax_.Z()));
+    }
+
+    Ogre::AxisAlignedBox AxisAlignedBox::FrostToOgre(const AxisAlignedBox& mBox)
+    {
+        return Ogre::AxisAlignedBox(Vector::FrostToOgre(mBox.GetMin()), Vector::FrostToOgre(mBox.GetMax()));
+    }
+
+    AxisAlignedBox AxisAlignedBox::OgreToFrost(const Ogre::AxisAlignedBox& mBox)
+    {
+        return AxisAlignedBox(Vector::OgreToFrost(mBox.getMinimum()), Vector::OgreToFrost(mBox.getMaximum()));
+    }
+}
+
+
+
