f090f023e79173ceaa296db2d93633ea9e16b7b9
kalith <kalith@7870209e-198f-4d4d-96a2-d6e540253c14>
2009-12-06 16:39:03 +0000

    Removed the Template addon from BaseUI. Moved it to the AddOns and Editor directories. Modified Editor's Template addon.
    MenuBar addon :
    Improved the look of the addon.
    Color is now customizable from the MenuBar.Config.barColor value.
    Added the "Close" item to the "File" menu (implemented).
    Removed the Add menu.
    Removed "Copy" and "Paste" from the edit menu and replaced them by "Clone" and "Delete" (not implemented).

--- bin/Interface/Editor/MenuBar/Menus.lua
+++ bin/Interface/Editor/MenuBar/Menus.lua
@@ -1,21 +1,17 @@
 
 MenuBar:AddMenu("File");
 MenuBar:AddMenuItem("File", "New", "Ctrl-N");
-MenuBar:AddMenuItem("File", "Open", "Ctrl-O");
+MenuBar:AddMenuItem("File", "Open", "Ctrl-O"):SetScript("OnClick", function() Frost:LoadZoneFile("Zones/Test/Test.xml"); end);
 MenuBar:AddMenuItem("File", "Save", "Ctrl-S");
 MenuBar:AddMenuItem("File", "SaveAs");
+MenuBar:AddMenuItem("File", "Close", "Ctrl-W"):SetScript("OnClick", function() Frost:UnloadZone(); end);
 MenuBar:AddMenuItem("File", "Quit", "Esc."):SetScript("OnClick", function() Exit(); end);
 
 MenuBar:AddMenu("Edit");
 MenuBar:AddMenuItem("Edit", "Undo", "Ctrl-Z");
 MenuBar:AddMenuItem("Edit", "Redo", "Ctrl-Shift-Z");
-MenuBar:AddMenuItem("Edit", "Copy", "Ctrl-C");
-MenuBar:AddMenuItem("Edit", "Paste", "Ctrl-V");
-
-MenuBar:AddMenu("Add");
-MenuBar:AddMenuItem("Add", "Doodad", "Ctrl-D");
-MenuBar:AddMenuItem("Add", "Light", "Ctrl-L");
-MenuBar:AddMenuItem("Add", "Unit", "Ctrl-U");
+MenuBar:AddMenuItem("Edit", "Clone", "Ctrl-C");
+MenuBar:AddMenuItem("Edit", "Delete", "Del.");
 
 MenuBar:AddMenu("View");
 MenuBar:AddMenuCheckItem("View", "Wireframe", "F2", false):SetScript("OnClick", function() base:OnClick(); Frost:ToggleWireframeView(); end);
