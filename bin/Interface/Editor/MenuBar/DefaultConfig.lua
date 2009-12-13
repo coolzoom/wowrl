717c83eb747e72564e7d9362fd17fd8fc2ca12fb
kalith <kalith@7870209e-198f-4d4d-96a2-d6e540253c14>
2009-12-13 22:52:47 +0000

    Removed unused code.

--- bin/Interface/Editor/MenuBar/DefaultConfig.lua
+++ bin/Interface/Editor/MenuBar/DefaultConfig.lua
@@ -1,5 +1,4 @@
 AddOns.MenuBar = AddOn("MenuBar");
 AddOns.MenuBar.Config = {
     ["barHeight"] = 16,
-    ["barColor"] = Color(0.75, 0.75, 0.75, 0.8),
 };
--- bin/Interface/Editor/Templates/Template_Dialog.lua
+++
@@ -1,6 +0,0 @@
-Templates.Dialog = {};
-Templates.Dialog.Config = {
-    ["mainColor"]   = PackColor(0.75, 0.75, 0.75, 0.8),
-    ["titleColor"]  = PackColor(0.5, 0.5, 0.5, 0.8),
-    ["sliderColor"] = PackColor(0.2, 0.2, 0.2, 0.5),
-}
