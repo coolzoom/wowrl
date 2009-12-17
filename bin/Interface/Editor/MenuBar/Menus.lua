5716c49ef4f83fca5d51081db2d21e8ae537f20c
kalith <kalith@7870209e-198f-4d4d-96a2-d6e540253c14>
2009-12-17 18:20:48 +0000

    Removed debugging code from InputManager. Cleaned up MenuBar's dropdown code. Made MenuBar's dropdown hide when a menu item is clicked.

--- bin/Interface/Editor/MenuBar/Menus.lua
+++ bin/Interface/Editor/MenuBar/Menus.lua
@@ -3,11 +3,13 @@ MenuBar:AddMenu("File");
 MenuBar:AddMenuItem("File", "New", "Ctrl-N");
 MenuBar:AddMenuItem("File", "Open", "Ctrl-O"):SetScript("OnClick", function()
     Frost:LoadZoneFile("Zones/Test/Test.xml");
+    MenuBar:CloseCurrentDropdown();
 end);
 MenuBar:AddMenuItem("File", "Save", "Ctrl-S");
 MenuBar:AddMenuItem("File", "SaveAs");
 MenuBar:AddMenuItem("File", "Close", "Ctrl-W"):SetScript("OnClick", function()
     Frost:UnloadZone();
+    MenuBar:CloseCurrentDropdown();
 end);
 MenuBar:AddMenuItem("File", "Quit", "Esc."):SetScript("OnClick", function()
     Exit();
@@ -22,9 +24,11 @@ MenuBar:AddMenuItem("Edit", "Delete", "Del.");
 MenuBar:AddMenu("View");
 MenuBar:AddMenuCheckItem("View", "Wireframe", "F2", false):SetScript("OnClick", function ()
     Frost:ToggleWireframeView();
+    MenuBar:CloseCurrentDropdown();
 end);
 MenuBar:AddMenuCheckItem("View", "Shading", "F3", true):SetScript("OnClick", function ()
     Frost:ToggleShading();
+    MenuBar:CloseCurrentDropdown();
 end);
 MenuBar:AddMenuItem("View", "BGColor"):SetScript("OnClick", function ()
     ColorSelector:SetColor(Color(Frost:GetBackgroundColor()));
@@ -33,15 +37,13 @@ MenuBar:AddMenuItem("View", "BGColor"):SetScript("OnClick", function ()
         Frost:SetBackgroundColor(color:Unpack());
     end);
     ColorSelector:Show();
-    UIParent.activeDropdown:Hide();
-    UIParent.activeDropdown = nil;
+    MenuBar:CloseCurrentDropdown();
 end);
 
 MenuBar:AddMenu("Settings");
 MenuBar:AddMenuItem("Settings", "Options"):SetScript("OnClick", function ()
     ConfigMenu:Show();
-    UIParent.activeDropdown:Hide();
-    UIParent.activeDropdown = nil;
+    MenuBar:CloseCurrentDropdown();
 end);
 
 MenuBar:AddMenu("Help");
